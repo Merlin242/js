ea26230005256584b3a98be112983df7bb5eea31
Merlin242 <145098743+Merlin242@users.noreply.github.com>
2024-01-23 11:38:17 +0300

    Add files via upload

---
+++ js-1/main.js
@@ -0,0 +1,112 @@
+// let name = prompt("Как вас зовут?");
+
+// document.write("<h1>Привет, " + name + "!</h1>");
+
+// let a = 1, b = 1;
+
+// let c = ++a; 
+// let d = b++;
+// console.log(a,b,c)
+// ОТвет 222
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console(a,x)
+//6 12
+
+//Раблта с переменными
+//1
+// let num = 3;
+// alert(num);
+// console.log(num)
+//2
+// let a = 10;
+// let b = 2;
+
+
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a / b);
+
+
+//3
+// let c = 15;
+// let d = 2;
+
+// let result = c + d;
+
+// alert(result);
+//4
+// let a = 10;
+// let b = 2;
+// let c = 5;
+
+// let sum = a + b + c;
+
+// console.log("Сумма a, b и c: ", sum);
+//5
+// let a = 17;
+// let b = 10;
+// let c = a - b;
+// let d = 7;
+// let result = c + d;
+
+// console.log("Значение переменной result: ", result);
+//  Работат с стороками
+//1
+// let str = 'Привет, Мир!';
+// console.log(str);
+//2
+// let str1 = 'Привет, ';
+// let str2 = 'Мир!';
+// console.log(str1 + str2);
+//3
+//let name = 'John';
+// console.log(`Привет, ${name}!`);
+//4
+// let age = 25;
+// console.log(`Мне ${age} лет!`);
+// Prompt
+// let userName = prompt("Как вас зовут?");
+// alert(`Ваше имя ${userName}`);
+//2
+// let userNumber = prompt("Введите число:");
+// alert((userNumber, 2));
+// Обращение к символам  стороки
+// let str = 'abcde';
+// alert(str[0]); 
+// alert(str[2]); 
+// alert([4]); 
+// function timeCalculation() {
+
+//     const secondsInMinute = 60;
+ 
+   
+//     const minutesInHour = 60;
+   
+//     const hoursInDay = 24;
+// }
+//Практика
+// const now = new Date();
+
+
+// const hour = now.getHours();
+// const minute = now.getMinutes();
+// const second = now.getSeconds();
+
+// const formattedTime = `${hour}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`;
+
+// console.log(formattedTime);
+//
+// Create a variable and assign it a number
+// const number = 7;
+// const squaredNumber = number * number;
+// console.log(`The square of ${number} is ${squaredNumber}.`);
+
+// var num  = 47
+// num += 7;
+// console.log(num)
+//
+// var num = 10
+// num += 3
+// console.log(num)
